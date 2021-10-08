c706337dc08a7303bd1f29d532fdaf65dfc8d660
Lino_Mendes <lino.mendes.fisico@gmail.com>
2021-10-07 22:17:53 -0300

    Primeiro Commit

---
+++ principal.py
@@ -0,0 +1,15 @@
+from leilao.dominio import Usuario, Lance, Leilao
+
+gui = Usuario('Gui')
+yuri = Usuario('Yuri')
+
+lance_do_yuri = Lance(yuri, 100.0)
+lance_do_gui = Lance(gui, 150.0)
+
+leilao = Leilao('Celular')
+
+leilao.lances.append(lance_do_yuri)
+leilao.lances.append(lance_do_gui)
+
+for lance in leilao.lances:
+    print(f'O usuário {lance.usuario.nome} deu uma lance de {lance.valor}')
